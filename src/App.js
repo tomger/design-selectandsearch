969e2b4979eb9f6004caf5f1ff3d24241d9c1de7
tomg <tom.germeau@gmail.com>
2018-04-29 11:31:53 -0400

    adding multi select

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { List, Map } from 'immutable';
+
 
 const activities = ['Yoga', 'Pilates', 'Barre']
 function formatSelection(array) {
@@ -10,8 +12,8 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      selection: ['Yoga'],
-      dropdownVisible: false,
+      selection: List(['Yoga']),
+      dropdownVisible: true,
     }
   }
 
@@ -19,10 +21,21 @@ class App extends Component {
     this.setState({dropdownVisible:true});
   }
 
-  onSelectActivity = (activity) => {
+  onSingleSelectActivity = (activity) => {
     this.setState({
-      selection: [activity],
+      selection: List([activity]),
       dropdownVisible: false,
+    });
+  }
+
+  onSelectActivity = (activity) => {
+    let index = this.state.selection.findIndex(e => e === activity)
+    this.setState({
+      selection:
+        this.state.selection.includes(activity) ?
+          this.state.selection.delete(index) :
+          this.state.selection.push(activity),
+      // dropdownVisible: false,
     });
   }
 
@@ -39,7 +52,15 @@ class App extends Component {
         <div className="dropdown" style={dropdownStyle}>
           {
             activities.map(v => (
-              <div onClick={(e) => this.onSelectActivity(v)}>{v}</div>
+              <div className="list-item" key={v}>
+                <div
+                  style={{flex:1}}
+                  onClick={(e) => this.onSingleSelectActivity(v)}>{v}</div>
+                <input
+                  type="checkbox"
+                  checked={ this.state.selection.includes(v) }
+                  onClick={(e) => this.onSelectActivity(v)}/>
+              </div>
             ))
           }
         </div>
